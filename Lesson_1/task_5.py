82f58677101d635854bd0912f36a0f175139d81c
morozov.da <morozov.da@gmail.com>
2021-12-26 22:33:09 +0300

    task-5

--- Lesson_1/task_5.py
+++ Lesson_1/task_5.py
@@ -7,3 +7,26 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+
+import subprocess
+import chardet
+
+yandex_ping = ['ping', 'yandex.ru']
+youtube_ping = ['ping', 'youtube.com']
+line_count = 0
+# host_count = 0
+
+yandex = subprocess.Popen(yandex_ping, stdout=subprocess.PIPE)
+youtube = subprocess.Popen(youtube_ping, stdout=subprocess.PIPE)
+hosts = [yandex, youtube]
+
+
+for host in hosts:
+	for line in host.stdout:
+		result = chardet.detect(line)
+		line = line.decode(result['encoding']).encode('utf-8')
+		print(line.decode('utf-8'))
+		line_count += 1
+		if line_count > 5-1:
+			break
+	line_count = 0
