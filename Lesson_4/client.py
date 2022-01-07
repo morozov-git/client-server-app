5da274c8316aefa1fb759babe3f412854cbe2d29
morozov.da <morozov.da@gmail.com>
2022-01-07 03:10:47 +0300

    Lesson_4_server_tests

--- Lesson_4/client.py
+++ Lesson_4/client.py
@@ -4,6 +4,7 @@ import sys
 import json
 import socket
 import time
+
 from Lesson_4.common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
     RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
 from Lesson_4.common.utils import get_message, send_message
---
+++ Lesson_4/unit_tests/server_tests.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import unittest
+# sys.path.append(os.path.join(os.getcwd(), '..'))
+from Lesson_4.common.variables import RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION, PRESENCE
+from Lesson_4.server import ServerApp
+
+class TestServerApp(unittest.TestCase):
+	pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
