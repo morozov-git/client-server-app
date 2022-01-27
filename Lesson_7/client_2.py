26009153de4ae0ad85c575828ae5883cf58edf44
morozov.da <morozov.da@gmail.com>
2022-01-27 22:31:06 +0300

    Lesson_7_added client2(with UserName)

--- Lesson_7/client_2.py
+++ Lesson_7/client_2.py
@@ -104,10 +104,12 @@ class ClientApp:
         parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
         parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
         parser.add_argument('-m', '--mode', default='listen', nargs='?')
+        parser.add_argument('-u', '--user', default='Guest', nargs='?')
         namespace = parser.parse_args(sys.argv[2:])
         server_address = namespace.addr
         server_port = namespace.port
         client_mode = namespace.mode
+        client_name = namespace.user
 
         # проверим подходящий номер порта
         if not 1023 < server_port < 65536:
@@ -119,13 +121,13 @@ class ClientApp:
             CLIENT_LOGGER.critical(f'Указан недопустимый режим работы {client_mode}, '
                                    f'допустимые режимы: listen , send')
             sys.exit(1)
-        return server_address, server_port, client_mode
+        return server_address, server_port, client_mode, client_name
 
     @classmethod
     def main(cls, *args, **kwargs):
         '''Загружаем параметы коммандной строки'''
         # client.py 192.168.0.100 8079
-        server_address, server_port, client_mode = ClientApp.arg_parser()
+        server_address, server_port, client_mode, client_name = ClientApp.arg_parser()
 
         CLIENT_LOGGER.info(f'Запущен клиент с парамертами: адрес сервера: {server_address}, '
                     f'порт: {server_port}, режим работы: {client_mode}')
@@ -194,7 +196,7 @@ class ClientApp:
                 # режим работы - отправка сообщений
                 if client_mode == 'send':
                     try:
-                        send_message(transport, ClientApp.create_message(transport))
+                        send_message(transport, ClientApp.create_message(transport, client_name))
                     except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
                         CLIENT_LOGGER.error(f'Соединение с сервером {server_address} было потеряно.')
                         sys.exit(1)
@@ -214,4 +216,4 @@ if __name__ == '__main__':
     ClientApp = ClientApp()
     ClientApp.main()
 
-# client.py 192.168.0.49 8888
+# client.py 192.168.0.49 8888 -m send -u TestSender
